59797d46228179249bb1dbd52f3c5d814137b83a
Diego Manuel Jaeger Rapoport <diegomrapoport@gmail.com>
2025-02-08 18:35:04 -0300

    solve input to get hero by id
    - Use withComponentInputBinding on config so it gets the path param
    through input
    - Set hero$ on onInit to be able to capture the id first

--- src/app/features/heroes/pages/dc-hero-details/dc-hero-details.component.ts
+++ src/app/features/heroes/pages/dc-hero-details/dc-hero-details.component.ts
@@ -1,16 +1,22 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { HeroItemComponent } from '../../components/hero-item.component';
 import { HeroService } from '../../services/hero.service';
 import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+import { Hero } from '../../models/hero.model';
 
 @Component({
   selector: 'app-dc-hero-details',
   templateUrl: './dc-hero-details.component.html',
   imports: [HeroItemComponent, AsyncPipe],
 })
-export class DcHeroDetailsComponent {
+export class DcHeroDetailsComponent implements OnInit {
   heroService = inject(HeroService);
 
   dcId = input<string>();
-  hero$ = this.heroService.getHeroById(this.dcId()!);
+  hero$: Observable<Hero> | undefined
+
+  ngOnInit(): void {
+      this.hero$ = this.heroService.getHeroById(this.dcId()!);
+  }
 }
