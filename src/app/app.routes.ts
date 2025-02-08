21c5350878aaf6f1bb66d26d3492f8b6beb45335
Diego Manuel Jaeger Rapoport <diegomrapoport@gmail.com>
2025-02-08 17:46:11 -0300

    remove app component from empty route
    This way it doesn't get duplicated on the html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { DCHeroListComponent } from './features/heroes/pages/dc-hero-list/dc-hero-list.component';
 import { MarvelHeroListComponent } from './features/heroes/pages/marvel-hero-list/marvel-hero-list.component';
-import { AppComponent } from './app.component';
 import { HeroFormComponent } from './features/heroes/pages/hero-form/hero-form.component';
 import { DcHeroDetailsComponent } from './features/heroes/pages/dc-hero-details/dc-hero-details.component';
 import { MarvelHeroDetailsComponent } from './features/heroes/pages/marvel-hero-details/marvel-hero-details.component';
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'dc-heroes', component: DCHeroListComponent },
   { path: 'marvel-heroes', component: MarvelHeroListComponent },
   { path: 'hero-form', component: HeroFormComponent },
